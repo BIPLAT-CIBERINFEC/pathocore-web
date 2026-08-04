35badac38f9527b9ec032c51b99dd5307492b6d7
Daniel-VM <danielvmillares@gmail.com>
2026-08-04 14:05:41 +0200

    fixed proxy_host values in production

--- src/pages/api/pathocore/v1/[...path].ts
+++ src/pages/api/pathocore/v1/[...path].ts
@@ -3,10 +3,12 @@ import { proxyApiRequest } from "@/lib/apiProxy";
 
 const PATHOCORE_API_PROXY_TARGET =
   process.env.PATHOCORE_API_PROXY_TARGET || "http://127.0.0.1:8000";
+const PATHOCORE_API_PROXY_HOST_HEADER =
+  process.env.PATHOCORE_API_PROXY_HOST_HEADER || "localhost";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   return proxyApiRequest(req, res, {
     targetBaseUrl: `${PATHOCORE_API_PROXY_TARGET}/v1`,
-    hostHeader: "localhost",
+    hostHeader: PATHOCORE_API_PROXY_HOST_HEADER,
   });
 }
